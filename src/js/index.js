16810e13d388e4e66d690eb0bafbd4dc02acbb6c
YouSooA <kinsk2839@naver.com>
2023-04-25 09:58:13 +0900

    refactor: 데이터 요청 Promise.all로 처리

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,22 @@
-import { domUtils, dataUtils } from './utils/index.js';
+import { domUtils } from './utils/index.js';
 import Header from './components/header/index.js';
 import Nav from './components/nav/index.js';
 import MainContent from './components/mainContent/index.js';
 
 const app = async ($targetEle) => {
-  const { getData } = dataUtils;
-  const { leftNavBarHeadLines, rightNavBarHeadLines } = await getData('http://localhost:3001/navBarData');
-  const pressData = await getData('http://localhost:3001/mainContentData');
+  const navBarUrl = 'http://localhost:3001/navBarData';
+  const pressUrl = 'http://localhost:3001/mainContentData';
+
+  const responses = await Promise.all([navBarUrl, pressUrl].map((url) => fetch(url)));
+  const [navBarData, pressData] = await Promise.all(responses.map((res) => res.json()));
+
+  const { leftNavBarHeadLines, rightNavBarHeadLines } = navBarData;
 
   const header = new Header($targetEle);
-  const nav = new Nav($targetEle, { leftNavBarHeadLines, rightNavBarHeadLines });
+  const nav = new Nav($targetEle, {
+    leftNavBarHeadLines,
+    rightNavBarHeadLines
+  });
   const mainContent = new MainContent($targetEle, { pressData });
 
   header.render();
